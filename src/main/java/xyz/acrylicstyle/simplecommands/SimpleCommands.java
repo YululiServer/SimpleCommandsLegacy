ab5977932488bb8c8bfda74fcb1f1d7d0e21ca56
acrylic-style <me@acrylicstyle.xyz>
2020-05-07 22:23:20 +0900

    add //texture

--- src/main/java/xyz/acrylicstyle/simplecommands/SimpleCommands.java
+++ src/main/java/xyz/acrylicstyle/simplecommands/SimpleCommands.java
@@ -1,11 +1,16 @@
 package xyz.acrylicstyle.simplecommands;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 import xyz.acrylicstyle.simplecommands.commands.Ping;
 import xyz.acrylicstyle.simplecommands.commands.PingAll;
 import xyz.acrylicstyle.simplecommands.commands.Suicide;
+import xyz.acrylicstyle.simplecommands.commands.Texture;
 
 import java.util.List;
 import java.util.Objects;
@@ -24,10 +29,26 @@ public class SimpleCommands extends JavaPlugin implements Listener {
         if (!disabledCommands.contains("ping")) Objects.requireNonNull(Bukkit.getPluginCommand("ping")).setExecutor(new Ping());
         if (!disabledCommands.contains("pingall")) Objects.requireNonNull(Bukkit.getPluginCommand("pingall")).setExecutor(new PingAll());
         if (!disabledCommands.contains("suicide")) Objects.requireNonNull(Bukkit.getPluginCommand("suicide")).setExecutor(new Suicide());
+        Objects.requireNonNull(Bukkit.getPluginCommand("textures")).setExecutor(new Texture());
         Bukkit.getPluginManager().registerEvents(this, this);
     }
 
     public static SimpleCommands getInstance() {
         return instance;
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        if (!SimpleCommands.getInstance().getConfig().getBoolean("autoResourcePack", false)) return;
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                String texture = SimpleCommands.getInstance().getConfig().getString("texture");
+                if (texture == null) return;
+                e.getPlayer().setResourcePack(texture);
+                e.getPlayer().sendMessage(ChatColor.GREEN + "リソースパックが変更されました。元に戻すには、"
+                        + ChatColor.YELLOW + "/texture clear" + ChatColor.GREEN + "を入力してください。");
+            }
+        }.runTaskLater(this, 20*3);
     }
 }
---
+++ src/main/java/xyz/acrylicstyle/simplecommands/commands/Texture.java
@@ -0,0 +1,75 @@
+package xyz.acrylicstyle.simplecommands.commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+import util.ICollectionList;
+import xyz.acrylicstyle.simplecommands.SimpleCommands;
+import xyz.acrylicstyle.simplecommands.utils.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class Texture implements CommandExecutor {
+    public static Sound BLOCK_NOTE_PLING;
+
+    static {
+        if (ICollectionList.asList(Sound.values()).map(Enum::name).contains("BLOCK_NOTE_BLOCK_PLING")) {
+            BLOCK_NOTE_PLING = Sound.valueOf("BLOCK_NOTE_BLOCK_PLING");
+        } else if (ICollectionList.asList(Sound.values()).map(Enum::name).contains("BLOCK_NOTE_PLING")) {
+            BLOCK_NOTE_PLING = Sound.valueOf("BLOCK_NOTE_PLING");
+        } else {
+            BLOCK_NOTE_PLING = Sound.valueOf("NOTE_PLING");
+        }
+    }
+
+    public static final List<UUID> confirm = new ArrayList<>();
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(ChatColor.RED + "This command cannot be invoked from console.");
+            return true;
+        }
+        if (args.length != 0 && args[0].equalsIgnoreCase("clear")) {
+            ((Player) sender).setResourcePack("https://google.com"); // invalid url will clear resource pack
+            return true;
+        }
+        String texture = SimpleCommands.getInstance().getConfig().getString("texture");
+        if (texture == null) return true;
+        Player player = (Player) sender;
+        if (args.length != 0 && player.isOp() && args[0].equalsIgnoreCase("all")) {
+            Utils.getOnlinePlayers().forEach(p -> p.setResourcePack(texture));
+            return true;
+        }
+        if (confirm.contains(player.getUniqueId())) {
+            player.setResourcePack(texture);
+            player.sendMessage(ChatColor.GREEN + "リソースパックを適用しました。");
+            player.sendMessage(ChatColor.GREEN + "元に戻す場合は" + ChatColor.YELLOW + "/texture clear" + ChatColor.GREEN + "を使用してください。");
+            confirm.remove(player.getUniqueId());
+        } else {
+            player.sendMessage("" + ChatColor.GOLD + ChatColor.BOLD + "==============================");
+            player.sendMessage("");
+            player.sendMessage("" + ChatColor.GOLD  + ChatColor.BOLD + "(!) " + ChatColor.RESET + ChatColor.GOLD
+                    + "リソースパックを読み込みますか？");
+            player.sendMessage(ChatColor.GOLD + "読み込む場合は、10秒以内に"
+                    + ChatColor.YELLOW + "/texture" + ChatColor.GOLD +"をもう一度打ってください。");
+            player.sendMessage("");
+            player.sendMessage("" + ChatColor.GOLD + ChatColor.BOLD + "==============================");
+            player.playSound(player.getLocation(), BLOCK_NOTE_PLING, 100, 1);
+            confirm.add(player.getUniqueId());
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    confirm.remove(player.getUniqueId());
+                }
+            }.runTaskLater(SimpleCommands.getInstance(), 20*10);
+        }
+        return true;
+    }
+}
